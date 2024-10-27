eaff4e5be54188b7e7c42b9cc263784cc7a7d7aa
3lacker <lakhalachraf0@gmail.com>
2024-10-27 21:36:21 +0100

    ERS-4 Créer une entitie Participant

---
+++ EventReservationSystem/src/main/java/com/ENSA/EventReservationSystem/model/Participant.java
@@ -0,0 +1,30 @@
+package com.ENSA.EventReservationSystem.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Participant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+}
